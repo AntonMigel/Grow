c68fefb7a63ab2885d316938fcf2496d87101d73
Naton <anton.migel.99@mail.ru>
2020-03-10 19:41:44 +0300

    Task Dev-1

---
+++ DEV_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DEV_1
+{  
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите последовательность:");
+            string line = Console.ReadLine();
+            int result = 1, number = 1;
+            for (int i = 0; i < line.Length - 1; i++)
+            {
+                if (line[i] == line[i + 1])
+                {
+                    number++;
+                    if (number > result)
+                    {
+                        result = number;
+                    }
+                }
+                else
+                {
+                    number = 1;
+                }
+            }
+            Console.WriteLine($"Максимальное количество одинаковых последовательных символов в строке:{result}");
+        }
+    }
+}
